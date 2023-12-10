e8b43d51702bfe3db3d6ba7518270e97bd20c6ce
samuelfilho-dev <snamuelfilho2011@gmail.com>
2023-12-10 18:41:19 -0300

    feat: add ost in cutscene

--- script/dialog_Box.gd
+++ script/dialog_Box.gd
@@ -4,9 +4,11 @@ extends NinePatchRect
 @onready var timer := $Timer as Timer
 
 var msg_queue: Array = [
-	"BBCode [color=green]test[/color]",
-	"Olá, Tudo Bem",
-	"Essa mesagem vai ser como exemplo"
+	"A sala está imersa em uma penumbra reconfortante,com o suave zumbido dos equipamentos médicos preenchendo o silêncio. A respiração de Elie é calma, mas seus olhos permanecem fechados. Os monitores ao lado da cama começam a emitir sons intermitentes, indicando um aumento nos sinais vitais de Elie.\n [right]Aperta espaço para continuar[/right]",
+	"Subitamente, os olhos de Elie se abrem abruptamente. Seu olhar é confuso e desorientado. Ela pisca várias vezes, tentando entender o que está acontecendo. \n [right]Aperta espaço para continuar[/right]",
+	"[b]Elie[/b]: Onde... onde estou? \n \n [right]Aperta espaço para continuar[/right]",
+	"Ela tenta mover-se, mas seus músculos parecem fracos. Os olhos de Elie examinam o quarto, buscando pistas sobre sua localização. As cortinas estão parcialmente abertas, revelando um vislumbre de luz noturna urbana do lado de fora. \n [right]Aperta espaço para continuar[/right]",
+	"O Ano é 2024 e Elie acorda de um coma no [b]Hospital Santa Ana[/b]. \n \n [right]Aperta espaço para continuar[/right]",
 ]
 
 func _input(event):
@@ -27,7 +29,6 @@ func show_message():
 	
 	text.visible_characters = 0
 	text.bbcode_text = _msg
-	print(msg_queue)
 	timer.start()
 
 	
