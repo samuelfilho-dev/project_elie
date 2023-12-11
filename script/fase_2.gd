743af26d4cbf51b0985d0b218172a4570527f3ec
samuelfilho-dev <snamuelfilho2011@gmail.com>
2023-12-11 02:09:31 -0300

    feat: add bonfire in fase 2

--- script/fase_2.gd
+++ script/fase_2.gd
@@ -9,3 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_exit_area_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://cenas/credits.tscn")
